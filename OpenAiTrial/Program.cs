6b7de76f67c7c96dab5f943b6c6af3c1405fb943
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-11-10 17:30:08 +0300

    Add project files.

---
+++ OpenAiTrial/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+
+
+var openAIService = new OpenAIService(new OpenAiOptions()
+{
+    ApiKey = "API KEY"
+});
+
+
+Console.WriteLine("Prompt Giriniz");
+var  usersPrompt=Console.ReadLine();
+
+var completionResult = await openAIService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+{
+    Messages = new List<ChatMessage>
+    {
+        ChatMessage.FromUser(usersPrompt)
+    },
+    Model = Models.Gpt_3_5_Turbo,
+    MaxTokens = 1000,//optional
+});
+if (completionResult.Successful)
+{
+    Console.WriteLine(completionResult.Choices.First().Message.Content);
+}
+Console.ReadLine();
