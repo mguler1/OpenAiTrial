b0c5d7fef3dcdb95a450b751f8cc25a2809bf67b
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-11-10 17:32:17 +0300

    update

--- OpenAiTrial/Program.cs
+++ OpenAiTrial/Program.cs
@@ -9,21 +9,25 @@ var openAIService = new OpenAIService(new OpenAiOptions()
     ApiKey = "API KEY"
 });
 
-
-Console.WriteLine("Prompt Giriniz");
-var  usersPrompt=Console.ReadLine();
-
-var completionResult = await openAIService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+while (true)
 {
-    Messages = new List<ChatMessage>
+    Console.WriteLine("Prompt Giriniz");
+    var usersPrompt = Console.ReadLine();
+
+    var completionResult = await openAIService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+    {
+        Messages = new List<ChatMessage>
     {
         ChatMessage.FromUser(usersPrompt)
     },
-    Model = Models.Gpt_3_5_Turbo,
-    MaxTokens = 1000,//optional
-});
-if (completionResult.Successful)
-{
-    Console.WriteLine(completionResult.Choices.First().Message.Content);
+        Model = Models.Gpt_3_5_Turbo,
+        MaxTokens = 1000,//optional
+
+
+    });
+    if (completionResult.Successful)
+    {
+        Console.WriteLine(completionResult.Choices.First().Message.Content);
+    }
+    Console.ReadLine();
 }
-Console.ReadLine();
